cbc1e970d0a5d3661d785b8c289c3b5779caa8f4
Shubham Prateek <sprateek2338@gmail.com>
2023-06-24 19:00:52 +0530

    Added Spring Boot files

---
+++ src/test/java/com/example/message_queue/MessageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.message_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
